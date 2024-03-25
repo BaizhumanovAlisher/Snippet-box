7dbff1f54c96aab30a3069bf6f9e45ebf561d18b
Baizhumanov Alisher <nebulous4707@gmail.com>
2024-03-25 18:31:10 +0500

    add handlers for auth

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -96,3 +96,28 @@ type snippetCreateForm struct {
 	Expires             int    `form:"expires"`
 	validator.Validator `form:"-"`
 }
+
+func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
+	//todo
+	fmt.Fprintln(w, "Display a HTML form for signing up a new user...")
+}
+
+func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
+	//todo
+	fmt.Fprintln(w, "Create a new user...")
+}
+
+func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
+	//todo
+	fmt.Fprintln(w, "Display a HTML form for logging in a user...")
+}
+
+func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
+	//todo
+	fmt.Fprintln(w, "Authenticate and login the user...")
+}
+
+func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
+	//todo
+	fmt.Fprintln(w, "Logout the user...")
+}
